cd51cddaae3823fcd6a91b1d756ed0bd02c95ded
thematho <thematho@gmail.com>
2015-03-21 20:56:43 -0300

    Added newrelic plugin for heroku apps

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 // This web.js file is used to deploy the application throught ExpressJS
+require('newrelic');
 var gzippo = require('gzippo');
 var express = require('express');
+
 var app = express();
 
 app.use(gzippo.staticGzip('' + __dirname + '/dist'));
