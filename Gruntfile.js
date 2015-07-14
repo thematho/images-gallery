58d7876eb53685cf0546067ffef2030f87a92a75
thematho <thematho@gmail.com>
2015-07-13 22:20:16 -0300

    Updated mock data to use lorem generated text
    Added lorem example text
    Added susy dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -191,6 +191,7 @@ module.exports = function(grunt) {
     // Compiles Sass to CSS and generates necessary files if requested
     compass: {
       options: {
+        require: 'susy',
         sassDir: '<%= yeoman.app %>/styles',
         cssDir: '.tmp/styles',
         generatedImagesDir: '.tmp/images/generated',
--- app/scripts/mocks/modules/image-service-mock.js
+++ app/scripts/mocks/modules/image-service-mock.js
@@ -21,220 +21,220 @@ angular.module('imagesGalleryMocks.imageService', [])
 angular.module('imagesGalleryMocks.imageService')
   .constant('MOCKED_IMAGES', {
     images: [{
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Lorem.',
+      description: 'Lorem ipsum dolor.',
       imageUrl: '/images/image-service/1.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Nostrum',
+      description: 'Unde voluptate, aliquid.',
       imageUrl: '/images/image-service/2.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Tempore',
+      description: 'Delectus, iure, optio.',
       imageUrl: '/images/image-service/3.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Cumque',
+      description: 'Magni, amet, cupiditate.',
       imageUrl: '/images/image-service/4.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sapiente',
+      description: 'Libero, dolorem corporis.',
       imageUrl: '/images/image-service/5.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Laboriosam',
+      description: 'Sunt, recusandae, molestiae!',
       imageUrl: '/images/image-service/6.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ratione.',
+      description: 'Natus et, temporibus.',
       imageUrl: '/images/image-service/7.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Iusto',
+      description: 'Omnis, quidem delectus.',
       imageUrl: '/images/image-service/8.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Aut.',
+      description: 'Facilis, repellendus dolorum?',
       imageUrl: '/images/image-service/9.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Voluptas.',
+      description: 'Est, autem, voluptatum!',
       imageUrl: '/images/image-service/10.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Pariatur.',
+      description: 'Enim, est, sequi!',
       imageUrl: '/images/image-service/11.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Fugiat.',
+      description: 'Aut ex, molestiae!',
       imageUrl: '/images/image-service/12.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Possimus!',
+      description: 'Perspiciatis, maiores animi!',
       imageUrl: '/images/image-service/13.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Error!',
+      description: 'Neque, aperiam, dicta?',
       imageUrl: '/images/image-service/14.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Vel.',
+      description: 'Omnis, ipsa, magnam.',
       imageUrl: '/images/image-service/15.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ex.',
+      description: 'Veniam, autem, aliquam!',
       imageUrl: '/images/image-service/16.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ex.',
+      description: 'Repudiandae, laborum est.',
       imageUrl: '/images/image-service/17.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Possimus?',
+      description: 'Doloremque, esse praesentium?',
       imageUrl: '/images/image-service/18.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ipsum.',
+      description: 'Quis fugit, dolore!',
       imageUrl: '/images/image-service/19.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sint.',
+      description: 'Quis, ipsa, consectetur?',
       imageUrl: '/images/image-service/20.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Deserunt.',
+      description: 'Sint, ut, libero.',
       imageUrl: '/images/image-service/21.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Nihil.',
+      description: 'Ab, ducimus, ipsa.',
       imageUrl: '/images/image-service/22.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Inventore!',
+      description: 'Distinctio, amet ut!',
       imageUrl: '/images/image-service/23.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Repellat?',
+      description: 'Consequatur qui, libero!',
       imageUrl: '/images/image-service/24.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Iusto.',
+      description: 'Asperiores harum, sint.',
       imageUrl: '/images/image-service/25.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Provident.',
+      description: 'Neque, enim, distinctio.',
       imageUrl: '/images/image-service/26.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sunt.',
+      description: 'Numquam sed, inventore.',
       imageUrl: '/images/image-service/27.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Obcaecati!',
+      description: 'Molestiae deleniti, itaque.',
       imageUrl: '/images/image-service/28.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Nesciunt!',
+      description: 'Itaque, ex expedita?',
       imageUrl: '/images/image-service/29.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Nulla!',
+      description: 'Odio, distinctio, officia!',
       imageUrl: '/images/image-service/30.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sit!',
+      description: 'Impedit, minima, magni.',
       imageUrl: '/images/image-service/31.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sequi?',
+      description: 'Enim, laboriosam earum.',
       imageUrl: '/images/image-service/32.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Minima?',
+      description: 'Sapiente, odio, repellendus?',
       imageUrl: '/images/image-service/33.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Veritatis.',
+      description: 'Ducimus, et? Consequatur.',
       imageUrl: '/images/image-service/34.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sapiente.',
+      description: 'Beatae, nostrum. Asperiores?',
       imageUrl: '/images/image-service/35.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sed!',
+      description: 'Corporis, dolorem, ducimus?',
       imageUrl: '/images/image-service/36.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Hic.',
+      description: 'Natus, iusto, voluptatem!',
       imageUrl: '/images/image-service/37.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Suscipit.',
+      description: 'Voluptate, voluptates laboriosam.',
       imageUrl: '/images/image-service/38.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Fuga!',
+      description: 'Temporibus, illo dolore.',
       imageUrl: '/images/image-service/39.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Praesentium.',
+      description: 'Deleniti pariatur, dolores?',
       imageUrl: '/images/image-service/40.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Perspiciatis!',
+      description: 'Ducimus, nemo, animi!',
       imageUrl: '/images/image-service/41.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Culpa.',
+      description: 'Odit, amet, rem.',
       imageUrl: '/images/image-service/42.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Quasi.',
+      description: 'Similique obcaecati, eos.',
       imageUrl: '/images/image-service/43.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Quos.',
+      description: 'Velit, unde, quidem.',
       imageUrl: '/images/image-service/44.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Sed.',
+      description: 'Quas, voluptatem, deleniti!',
       imageUrl: '/images/image-service/45.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Repudiandae!',
+      description: 'Repudiandae, veritatis, quis.',
       imageUrl: '/images/image-service/46.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Aliquid?',
+      description: 'Ullam, odit, dolore.',
       imageUrl: '/images/image-service/47.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ipsa.',
+      description: 'Exercitationem, voluptatum, nesciunt.',
       imageUrl: '/images/image-service/48.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Rerum.',
+      description: 'Cupiditate, quasi officia.',
       imageUrl: '/images/image-service/49.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Eum.',
+      description: 'Dolorem, earum, non.',
       imageUrl: '/images/image-service/50.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Minima.',
+      description: 'Animi, error, ipsam.',
       imageUrl: '/images/image-service/51.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Rerum?',
+      description: 'Alias, dicta, impedit.',
       imageUrl: '/images/image-service/52.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Ratione!',
+      description: 'Ipsum, eaque, officia!',
       imageUrl: '/images/image-service/53.jpg'
     }, {
-      owner: 'Sample Owner',
-      description: 'Image description',
+      owner: 'Rerum',
+      description: 'Assumenda, placeat, id.',
       imageUrl: '/images/image-service/54.jpg'
     }]
   });
